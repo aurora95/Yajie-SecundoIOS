d76ce663f3c450bbdc6830f4c4479de54fdd5f4f
YajieXing <YajieXing@shengchunyanmatoMacBook-Pro.local>
2015-09-25 17:07:17 +0800

    Initial Commit

---
+++ SecundoIOSTests/SecundoIOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  SecundoIOSTests.swift
+//  SecundoIOSTests
+//
+//  Created by YajieXing on 15/9/25.
+//  Copyright (c) 2015年 Yajie Xing. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SecundoIOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
